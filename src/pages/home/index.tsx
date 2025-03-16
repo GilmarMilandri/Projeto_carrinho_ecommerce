e02b349357347c13f1b18c9e290f3e8c6ffd0f3b
Gilmar Milandri <gilmarmilandri11@gmail.com>
2025-03-15 23:10:18 -0400

    Api consumida

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,28 @@
 
+import { use, useEffect, useState } from "react"
 import { BsCartPlus } from "react-icons/bs"
+import { api } from "../../services/api"
+
+interface ProductProps{
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  cover: string;
+}
 
 export function Home() {
+
+  const [products, setProducts] = useState<ProductProps[]>([])
+
+useEffect(() => {
+  async function getProducts(){
+    const response = await api.get("/products")
+    setProducts(response.data)
+  }
+
+  getProducts();
+}, [])
 
     return (
       <div>
@@ -9,17 +30,21 @@ export function Home() {
           <h1 className="font-bold text-2xl mb-4 mt-10 text-center">Produtos em alta</h1>
 
           <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-5">
-            <section className="w-full">
+            {products.map( (product) => (
+              <section key={product.id} className="w-full">
               <img 
               className="w-full rounded-lg max-h-70 mb-2"
-              src="https://http2.mlstatic.com/D_NQ_NP_630775-MLB71370076372_082023-O.webp" 
-              alt="Logo do produto" 
+              src={product.cover} 
+              alt={product.title} 
               />
-              <p className="font-medium mt-1 mb-2">Pc Gamer</p>
+              <p className="font-medium mt-1 mb-2">{product.title}</p>
 
               <div className="flex gap-3 items-center">
                 <strong className="text-zinc-700/90">
-                  R$ 1000
+                  {product.price.toLocaleString("pt-BR", {
+                    style: "currency",
+                    currency: "BRL"
+                  })}
                 </strong>
                 <button className="bg-zinc-900 p-1 rounded">
                   <BsCartPlus size={22} color="#FFF"/>
@@ -27,6 +52,7 @@ export function Home() {
               </div>
 
             </section>
+            ))}
           </div>
 
         </main>
