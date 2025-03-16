3ed6c87737af6680285169b71c37eefca41dd540
Gilmar Milandri <gilmarmilandri11@gmail.com>
2025-03-16 13:15:12 -0400

    finalizando carrinho

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,36 +1,52 @@
+import { useContext } from "react"
+
+import { CartContext } from "../../contexts/CartContext"
+import { Link } from "react-router-dom"
 
 export function Cart() {
+  const { cart } = useContext(CartContext)
 
     return (
       <div className="w-full max-w-7xl px-4 mx-auto">
         <h1 className="font-medium text-2xl text-center my-4"> Meu carrinho </h1>
 
-        <section className="flex items-center justify-between border-b-2 border-gray-300">
-        <img 
-              className="w-28"
-              src="https://http2.mlstatic.com/D_NQ_NP_630775-MLB71370076372_082023-O.webp" 
-              alt="Logo do produto" 
-              />
+        {cart.length === 0 && (
+          <div className="flex flex-col items-center justify-center">
+            <p className="font-medium">Ops Seu carrinho está vazio...</p>
+            <Link to="/" className="bg-slate-600 my-3 p-1 px-3 text-white font-medium rounded">
+              Acessar Produtos
+            </Link>
+          </div>
+        )}
 
-              <strong>Preço: R$ 1000</strong>
+        {cart.map( (item) => (
+          <section key={item.id} className="flex items-center justify-between border-b-2 border-gray-300">
+          <img 
+                className="w-28"
+                src={item.cover}
+                alt={item.title}
+                />
+  
+                <strong>{item.price}</strong>
+  
+                <div className="flex items-center justify-center gap-3">
+                  <button className="bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center">
+                    -
+                  </button>
+                  {item.amount}
+                  <button className="bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center">
+                    +
+                  </button>
+                </div>
+  
+                <strong className="float-right">
+                  SubTotal: {item.total}
+                </strong>
+  
+          </section>
+        ))}
 
-              <div className="flex items-center justify-center gap-3">
-                <button className="bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center">
-                  -
-                </button>
-                1
-                <button className="bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center">
-                  +
-                </button>
-              </div>
-
-              <strong className="float-right">
-                SubTotal: R$ 1.000
-              </strong>
-
-        </section>
-
-        <p className="font-bold mt-4">Total: R$ 1.000</p>
+        {cart.length !== 0 && <p className="font-bold mt-4">Total: R$ 1.000</p>}
       </div>
     )
   }
