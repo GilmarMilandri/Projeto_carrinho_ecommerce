4be15d25f6d20a1a86869cb4bfee6e3b89d1d3b9
Gilmar Milandri <gilmarmilandri11@gmail.com>
2025-03-15 22:43:43 -0400

    Criado interface da home e carrinho

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -2,9 +2,35 @@
 export function Cart() {
 
     return (
-      <h1>
-      Pagina carrinho de compras
-      </h1>
-  
+      <div className="w-full max-w-7xl px-4 mx-auto">
+        <h1 className="font-medium text-2xl text-center my-4"> Meu carrinho </h1>
+
+        <section className="flex items-center justify-between border-b-2 border-gray-300">
+        <img 
+              className="w-28"
+              src="https://http2.mlstatic.com/D_NQ_NP_630775-MLB71370076372_082023-O.webp" 
+              alt="Logo do produto" 
+              />
+
+              <strong>Preço: R$ 1000</strong>
+
+              <div className="flex items-center justify-center gap-3">
+                <button className="bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center">
+                  -
+                </button>
+                1
+                <button className="bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center">
+                  +
+                </button>
+              </div>
+
+              <strong className="float-right">
+                SubTotal: R$ 1.000
+              </strong>
+
+        </section>
+
+        <p className="font-bold mt-4">Total: R$ 1.000</p>
+      </div>
     )
   }
