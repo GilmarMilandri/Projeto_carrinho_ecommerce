0419d016fe153611a161bfd13a616137180667ca
Gilmar Milandri <gilmarmilandri11@gmail.com>
2025-03-18 14:26:27 -0400

    Pagina detalhes criada

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,11 @@
 
-import { useEffect, useState, useContext } from "react"
+import { useEffect, useState, useContext, FormEvent } from "react"
 import { BsCartPlus } from "react-icons/bs"
 import { api } from "../../services/api"
 import { CartContext } from "../../contexts/CartContext";
 import toast from "react-hot-toast";
+import { useNavigate } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 export interface ProductProps{
   id: number;
@@ -27,7 +29,7 @@ useEffect(() => {
   getProducts();
 }, [])
 
-  function handleAddCartItem(product: ProductProps){
+   function handleAddCartItem(product: ProductProps){
     toast.success("Produto adicionado no carrinho!", {
       style:{
         borderRadius: 10,
@@ -45,13 +47,19 @@ useEffect(() => {
 
           <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-5">
             {products.map( (product) => (
-              <section key={product.id} className="w-full">
+              <section 
+              key={product.id} 
+              className="w-full" 
+              style={{ cursor: "pointer"}}
+              >
+                <Link to={`/product/${product.id}`}>
               <img 
               className="w-full rounded-lg max-h-70 mb-2"
               src={product.cover} 
               alt={product.title} 
               />
               <p className="font-medium mt-1 mb-2">{product.title}</p>
+              </Link>
 
               <div className="flex gap-3 items-center">
                 <strong className="text-zinc-700/90">
@@ -60,7 +68,7 @@ useEffect(() => {
                     currency: "BRL"
                   })}
                 </strong>
-                <button className="bg-zinc-900 p-1 rounded" onClick={ () => handleAddCartItem(product) }>
+                <button className="bg-zinc-900 p-1 rounded cursor-pointer" onClick={ () => handleAddCartItem(product) }>
                   <BsCartPlus size={22} color="#FFF"/>
                 </button>
               </div>
